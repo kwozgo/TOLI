2c0f0dddd486a652ce388afa5adcff5083888f0d
Charlie Walton <103379122+kwozgo@users.noreply.github.com>
2022-05-04 01:28:04 +0300

    Update cell label using tag (UILabel)

--- TOLI/Modules/ChecklistViewController.swift
+++ TOLI/Modules/ChecklistViewController.swift
@@ -19,12 +19,29 @@ final class ChecklistViewController: UITableViewController {
 
 extension ChecklistViewController {
     
+    // TODO: Something
+    // FIXME: Fix some bugs
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+        }
+        
         return cell
     }
     
