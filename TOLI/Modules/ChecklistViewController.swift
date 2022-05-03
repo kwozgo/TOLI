3d59520cbcdb47fe4320b7247782a314267949aa
Charlie Walton <103379122+kwozgo@users.noreply.github.com>
2022-05-04 00:57:31 +0300

    Added data source for ChecklistViewController

--- TOLI/Modules/ChecklistViewController.swift
+++ TOLI/Modules/ChecklistViewController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+final class ChecklistViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
+
+// MARK: - Table View Data Source
+
+extension ChecklistViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+    
 }
