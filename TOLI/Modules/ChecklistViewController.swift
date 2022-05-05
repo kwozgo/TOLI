64226966761164c55b74f47b55e86f2c65d57184
Charlie Walton <103379122+kwozgo@users.noreply.github.com>
2022-05-05 16:57:09 +0300

    Configure checkmarks

--- TOLI/Modules/ChecklistViewController.swift
+++ TOLI/Modules/ChecklistViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 final class ChecklistViewController: UITableViewController {
+    let row0text = "Walk the dog"
+    let row1text = "Brush my teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +33,7 @@ extension ChecklistViewController {
     // TODO: Something
     // FIXME: Fix some bugs
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -31,17 +42,18 @@ extension ChecklistViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         
         if indexPath.row % 5 == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
         } else if indexPath.row % 5 == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
         } else if indexPath.row % 5 == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
         } else if indexPath.row % 5 == 3 {
-            label.text = "Soccer practice"
+            label.text = row3text
         } else if indexPath.row % 5 == 4 {
-            label.text = "Eat ice cream"
+            label.text = row4text
         }
         
+        self.configureCheckmark(for: cell, at: indexPath)
         return cell
     }
     
@@ -52,7 +64,26 @@ extension ChecklistViewController {
 extension ChecklistViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = self.tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
+            var isChecked = false
+            
+            if indexPath.row == 0 {
+                row0checked.toggle()
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                row1checked.toggle()
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                row2checked.toggle()
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                row3checked.toggle()
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                row4checked.toggle()
+                isChecked = row4checked
+            }
+            
+            if isChecked {//cell.accessoryType == .none {
                 cell.accessoryType = .checkmark
             } else {
                 cell.accessoryType = .none
@@ -60,5 +91,27 @@ extension ChecklistViewController {
         }
         
         self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    private func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
+        var isChecked = false
+        
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3 {
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
 }
