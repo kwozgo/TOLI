0fd3dc76a42d9890a09db7acc133e0217808a4d9
Charlie Walton <103379122+kwozgo@users.noreply.github.com>
2022-05-05 23:05:42 +0300

    Added cancel and done buttons with back functionality

---
+++ TOLI/Modules/AddItemViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddItemViewController.swift
+//  TOLI
+//
+//  Created by Evolution on 5/5/22.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.largeTitleDisplayMode = .never
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction private func done() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction private func cancel() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
